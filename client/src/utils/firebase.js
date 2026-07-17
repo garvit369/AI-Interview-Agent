5530fc4879e6b8dda0b02cd160f21c5eef9dca85
garvit369 <mgarvit314@gmail.com>
2026-07-18 01:59:08 +0530

    changed firebase configuration

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "ai-agent-841ac.firebaseapp.com",
-  projectId: "ai-agent-841ac",
-  storageBucket: "ai-agent-841ac.firebasestorage.app",
-  messagingSenderId: "360186065776",
-  appId: "1:360186065776:web:8417e56ba414c28587d5b5"
+  authDomain: "interviewiq-79e2a.firebaseapp.com",
+  projectId: "interviewiq-79e2a",
+  storageBucket: "interviewiq-79e2a.firebasestorage.app",
+  messagingSenderId: "164396017786",
+  appId: "1:164396017786:web:b3fcded138cd59de1333c3",
+  measurementId: "G-21RXJ44WWF"
 };
 
 // Initialize Firebase
