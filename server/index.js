b2e93ac13bbca2b6655be9a34255ccbe7505f53a
garvit369 <mgarvit314@gmail.com>
2026-07-18 01:34:28 +0530

    fixed cors error

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ import paymentRouter from './routes/payment.route.js'
 const app = express()
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: ["http://localhost:5173", "https://interviewiq-client-y2ss.onrender.com"],
+    credentials: true
 }))
 
 app.use(express.json())
