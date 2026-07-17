e4fa9c23cd38497a9d20e35ad74909391304a5d5
garvit369 <mgarvit314@gmail.com>
2026-07-18 01:00:08 +0530

    Change ServerUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import InterviewReport from './pages/InterviewReport'
 import Pricing from './pages/Pricing'
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://interviewiq-ai-g12q.onrender.com"
 
 function App() {
 
